9729900cf12c311e4231ef8204c3ac2649af3194
Casper Cromjongh <c.cromjongh@student.tudelft.nl>
2020-04-19 23:07:29 +0200

    Update eval_cityscapes_batch_resize.sh
    Use mogrify instead of convert.

--- eval_cityscapes_batch_resize.sh
+++ eval_cityscapes_batch_resize.sh
@@ -1,3 +1,5 @@
-#run this script at the top of the project 
-mkdir datasets/cityscapes_test
-convert "datasets/leftImg8bit/val/frankfurt_test/*.png" -set filename:base "%[basename]" -resize 256x256! "datasets/cityscapes_test/%[filename:base].png"
+# Run this script at the top of the project, where cityscapes is expected to contain original the cityscapes dataset
+mkdir ../cityscapes-converted
+mogrify -resize 256x256! -path ../cityscapes-converted/leftImg8bit/val/frankfurt/ ../cityscapes/leftImg8bit/val/frankfurt/*.png
+mogrify -resize 256x256! -path ../cityscapes-converted/leftImg8bit/val/lindau/ ../cityscapes/leftImg8bit/val/lindau/*.png
+mogrify -resize 256x256! -path ../cityscapes-converted/leftImg8bit/val/munster/ ../cityscapes/leftImg8bit/val/munster/*.png
